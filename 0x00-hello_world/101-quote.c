087116545d3a321cf8afa3cc51ed1df9530a7316
codelife2023 <talk2orismac@gmail.com>
2023-03-18 12:14:06 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
-* main - Entry point
-* Return: Always 0 (Success)
+* main - Entry
+* Return: Always 0
 */
 int main(void)
 {
-    
-fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+     
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 37);
 
 return (1);
 
