745b3a097f0b0d3dae52d3da2564064cc22cefc8
codelife2023 <talk2orismac@gmail.com>
2023-03-18 14:54:13 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
-* main - Entry
-* Return: Always 0
+* main - Entry 
+* Return: 1
 */
 int main(void)
 {
-     
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 37);
+	
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 
