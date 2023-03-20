441d11db4111926485e87f0011ff2f32343b2061
codelife2023 <talk2orismac@gmail.com>
2023-03-20 15:54:37 -0700

    hello

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,7 +13,6 @@
 int main(void)
 {
 	int num;
-
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
@@ -23,8 +22,6 @@ int main(void)
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
-
 return (0);
 }
